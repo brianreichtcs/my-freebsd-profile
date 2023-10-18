9c6285f17be788e66e2640440c9e6b811f6ad8c0
Brian Reich <brian.reich@thecoresolution.com>
2023-10-18 10:24:39 -0600

    fix: only elevate the things that need elevated

--- setup-profile.sh
+++ setup-profile.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-# This script is intended to be run using sudo
-if [ 0 -ne `id -u` ]
-then
-    echo "This script must be run with elevated privleges"
-    exit 1
-fi
-
 echo "Setting up Git defaults..."
 git config --global user.email "brian.reich@thecoresolution.com"
 git config --global user.name "Brian Reich"
 
 if ! [ -x "$(command -v zsh)" ]; then
     echo "ZSH is not installed. Installing"
-    pkg install -y zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sudo pkg install -y zsh
 fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 if ! [ -x "$(command -v nvim)" ]; then
     echo "Neovim is not installed. Installing"
-    pkg install -y neovim
+    sudo pkg install -y neovim
 fi
 
