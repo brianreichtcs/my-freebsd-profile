7e71c4fcd1bea123a2b2a2ff7248032a462f9004
Brian Reich <brian.reich@thecoresolution.com>
2023-10-19 09:00:34 -0600

    feat: autopush configuration and tmux config

--- setup-profile.sh
+++ setup-profile.sh
@@ -4,6 +4,7 @@ echo "Setting up Git defaults..."
 git config --global user.email "brian.reich@thecoresolution.com"
 git config --global user.name "Brian Reich"
 git config pull.rebase false
+git config --global --add --bool push.autoSetupRemote true
 
 if ! [ -x "$(command -v zsh)" ]; then
     echo "ZSH is not installed. Installing"
@@ -11,6 +12,17 @@ if ! [ -x "$(command -v zsh)" ]; then
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if ! [ -x "$(command -v tmux)" ]; then
+    echo "tmux not found. Installing..."
+    sudo pkg install -y tmux
+fi
+
+echo "Setting up tmux plugin manager..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installing my tmux configuration..."
+cp config/.tmux.conf ~
 
 if ! [ -x "$(command -v nvim)" ]; then
     echo "Neovim is not installed. Installing"
