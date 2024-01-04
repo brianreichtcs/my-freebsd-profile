1ea886b77ad0ce04e91570e2f37cf9af49fba89d
Brian Reich <brian.reich@thecoresolution.com>
2024-01-04 10:59:35 -0700

    feat: got a hello world example of luasnips going. yay.

--- config/.config/nvim/lua/plugins/luasnip.lua
+++ config/.config/nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,34 @@
 return {
     "L3MON4D3/LuaSnip",
     version = "v2.*",
-    dependencies = { "rafamadriz/friendly-snippets" }
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+        -- Load vscode style snippets from other plugins
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        -- Build our own snippets here
+        local ls = require "luasnip"
+        local s = ls.snippet
+        local t = ls.text_node
+        local i = ls.insert_node
+
+        ls.add_snippets("php", {
+            s("hello", {
+                t('print("hello world")')
+            })
+        })
+
+        -- Setup keymaps
+        vim.keymap.set(
+            {"i", "s"},
+            "<c-k>",
+            function()
+                if ls.expand_or_jumpable() then
+                    ls.expand_or_jump()
+                end
+            end,
+            { silent = true }
+        )
+    end
 }
 
