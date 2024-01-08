a4e74e04964567afcd243d43707573ea2910ff86
Brian Reich <brian.reich@thecoresolution.com>
2024-01-08 05:59:06 -0700

    fix: removing old config

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -16,6 +16,9 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- Show line numbers
+vim.cmd("set number")
+
 -- Mark line 80
 vim.cmd("set colorcolumn=80")
 
