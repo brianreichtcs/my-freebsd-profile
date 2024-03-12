516a24bd7fe564bffa53d44a68cd96de31ec6070
Brian Reich <brian.reich@thecoresolution.com>
2024-03-12 06:23:24 -0600

    feat: change neotree show/hide defaults

--- config/.config/nvim/lua/plugins/neotree.lua
+++ config/.config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,20 @@ return {
         "MunifTanjim/nui.nvim",
         "3rd/image.nvim"
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                -- Show filtered items
+                visible = true,
+                -- Show the number of hidden files
+                show_hidden_count = true,
+                -- Don't hide .files
+                hide_dot_files = false,
+                -- Don't hide files listed in .gitignore
+                hide_gitignored = false
+            }
+        }
+    },
     config = function()
         vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal left<CR>')
     end
